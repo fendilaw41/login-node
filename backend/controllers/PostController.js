dcc188782866adfef87b2674d9d3f6fd5f2aa015
fendilaw41 <fendilaw41@gmail.com>
2021-12-01 23:19:37 +0700

    fendi

--- backend/controllers/PostController.js
+++ backend/controllers/PostController.js
@@ -13,8 +13,7 @@ export const getAllPost = async(req, res) => {
     } catch (error) {
         res.json({
             status: false,
-            message: "Gagal Fetch Posts",
-            data: error
+            message: "Gagal Fetch Posts"
         });
     }
 }
@@ -22,6 +21,18 @@ export const getAllPost = async(req, res) => {
 export const storePost = async(req, res) => {
     const { title, slug, body, description } = req.body
     try {
+        if (!title) {
+            res.status(400).send({ message: "Post title can not be empty!"});
+            return;
+        }
+        if (!body) {
+            res.status(400).send({ message: "Post Body can not be empty!"});
+            return;
+        }
+        if (!description) {
+            res.status(400).send({ message: "Post Description can not be empty!"});
+            return;
+        }
         await Posts.create({
             title: title,
             slug: slug,
@@ -35,6 +46,75 @@ export const storePost = async(req, res) => {
         });
         
     } catch (error) {
-        console.log(error);
+       res.json({
+           status: false,
+           message: error.message
+        });
+    }
+}
+
+export const getPostById = async(req, res) => {
+    try {
+        const id = req.params.id;
+        const post = await Posts.findAll({
+            attributes: ['title', 'body', 'description'],
+            where: {
+                id: id
+            }
+        });
+        if(!post[0])
+        res.status(404).send({
+            message: "Posts with id " + id + " is not found."
+        });
+        else
+        res.json({
+            status: true,
+            message: "Success Post By Id",
+            data: post[0]
+        });
+        
+    } catch (error) {
+        res.json({
+            status: false,
+            message: error.message
+         });
+    }
+}
+
+export const updatePost = async(req, res) => {
+        const id = req.params.id;
+        const { title, slug, body, description } = req.body
+    try {
+        if (!title) {
+            res.status(400).send({ message: "Post title can not be empty!"});
+            return;
+        }
+        if (!body) {
+            res.status(400).send({ message: "Post Body can not be empty!"});
+            return;
+        }
+        if (!description) {
+            res.status(400).send({ message: "Post Description can not be empty!"});
+            return;
+        }
+        await Posts.update({ title: title, slug: slug, body: body, description: description }, {
+            where: { id: id } 
+        })
+        res.json({ status: true,  message: "Success Update Post" });
+        
+    } catch (error) {
+        res.json({  status: false, message: "Failed Fetch Posts", error: error.message });
+    }
+}
+
+export const deletePost = async(req, res) => {
+    try {
+        const id = req.params.id;
+        await Posts.destroy({ where: { id: id } });
+        
+        res.json({ status: true, message: "Success Post Deleted" });
+        
+    } catch (error) {
+        res.json({ status: false, message: error.message });
     }
 }
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { getAllPost, storePost } from "../controllers/PostController.js";
+import { getAllPost,
+         storePost,
+         getPostById,
+         updatePost,
+         deletePost
+} from "../controllers/PostController.js";
 import { refreshToken } from "../controllers/RefreshToken.js";
 import { getUsers, Register, Login, Logout } from '../controllers/UserController.js'
 import { verifyToken } from "../middleware/VerifyToken.js";
@@ -16,5 +21,8 @@ router.delete('/logout', Logout);
 router.get('/users', verifyToken, getUsers);
 router.get('/posts', getAllPost);
 router.post('/posts', storePost);
+router.get('/posts/:id', getPostById);
+router.patch('/posts/:id', updatePost);
+router.delete('/posts/:id', deletePost);
 
 export default router;
